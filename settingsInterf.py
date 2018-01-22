7fd1e800057e14a9dbe94e57de434a20e949eb0d
Camille Wiedemann <26042671+Camille-w@users.noreply.github.com>
2018-01-22 17:27:46 +0100

    ajout de la mise a jour du xml depuis l'interface

--- settingsInterf.py
+++ settingsInterf.py
@@ -1,8 +1,7 @@
 ############# definition de l'interface de creation de flash cards
 
 from PyQt5 import QtCore, QtWidgets, QtGui
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLineEdit, QLabel, QPushButton, QHBoxLayout, \
-    QProgressBar, QSlider, QComboBox, QFileDialog, QFrame
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLineEdit, QLabel, QPushButton, QHBoxLayout
 
 import sys
 
@@ -14,6 +13,17 @@ class mySettings(QWidget):
         ## la fenetre
         self.setObjectName("Current Settings")
         self.setFixedSize(497, 492)
+
+        ## le layout du haut
+        self.whatWidget = QWidget(self)
+        self.whatWidget.setGeometry(QtCore.QRect(10, 10, 451, 31))
+        self.togivename = QHBoxLayout(self.whatWidget)
+        self.togivename.setContentsMargins(0, 0, 0, 0)
+        self.explain = QLabel(self.whatWidget)
+        self.explain.setObjectName("explain")
+        self.explain.setText(" Vous pouvez mettre à jous les paramètres de l'application ci-dessous :")
+        self.togivename.addWidget(self.explain)
+
         ## le layout de la grille centrale
         self.gridWidget = QWidget(self)
         self.gridWidget.setGeometry(QtCore.QRect(10, 50, 451, 341))
@@ -26,24 +36,28 @@ class mySettings(QWidget):
         # ligne 1 : langue par défaut
         self.langue = QLabel(self.gridWidget)
         self.answergrid.addWidget(self.langue, 1, 0, 1, 3)
-        self.langue.setText(" Le language par défault :")
+        self.langue.setText("Language par défault :")
         self.editlangue = QLineEdit(self.gridWidget)
-        self.answergrid.addWidget(self.editlangue, 1, 1, 1, 3)
+        self.answergrid.addWidget(self.editlangue, 1, 2, 1, 2)
         self.editlangue.setText(currentSettings[0])
 
         # ligne 2 : user par défaut
         self.user = QLabel(self.gridWidget)
         self.answergrid.addWidget(self.user, 2, 0, 1, 3)
-        self.user.setText(" L'utilisateur en cours est : ")
+        self.user.setText("Utilisateur en cours : ")
         self.edituser = QLineEdit(self.gridWidget)
-        self.answergrid.addWidget(self.edituser, 2, 1, 1, 3)
+        self.answergrid.addWidget(self.edituser, 2, 2, 1, 2)
         self.edituser.setText(currentSettings[1])
 
         # tableau 2x5 sur un bloc 3x6 pour les parametre des jeux
+        # pour la forme
+        self.notvisible = QLineEdit(self.gridWidget)
+        self.answergrid.addWidget(self.notvisible, 3, 0, 1, 1)
+        #self.notvisible.setVisible(False)
         # les caractéristiques
         self.nbcards = QLabel(self.gridWidget)
         self.answergrid.addWidget(self.nbcards, 4, 0, 1, 1)
-        self.nbcards.setText("Nombre de cartes à jouer ")
+        self.nbcards.setText("Nombre de cartes")
         self.chrono = QLabel(self.gridWidget)
         self.answergrid.addWidget(self.chrono, 5, 0, 1, 1)
         self.chrono.setText("Temps de jeu")
@@ -63,32 +77,32 @@ class mySettings(QWidget):
             self.answergrid.addWidget(self.chronoEdit[rank], 5, rank+1, 1, 1)
             self.chronoEdit[rank].setText(currentSettings[3][rank])
 
-        ## le layout du haut
-        self.whatWidget = QWidget(self)
-        self.whatWidget.setGeometry(QtCore.QRect(10, 10, 451, 31))
-        self.togivename = QHBoxLayout(self.whatWidget)
-        self.togivename.setContentsMargins(0, 0, 0, 0)
-        self.explain = QLabel(self.whatWidget)
-        self.explain.setObjectName("explain")
-        self.explain.setText(" Vous pouvez mettre à jous les paramètres de l'application ci-dessous :")
-        self.togivename.addWidget(self.explain)
+        ## le bouton du bas
 
-        ## le layout du bas
-        self.bottomWidget = QWidget(self)
-        self.bottomWidget.setGeometry(QtCore.QRect(10, 400, 451, 32))
-        self.toUpdate = QHBoxLayout(self.bottomWidget)
-        self.toUpdate.setContentsMargins(0, 0, 0, 0)
+        #self.bottomWidget = QWidget(self)
+        #self.bottomWidget.setGeometry(QtCore.QRect(10, 400, 451, 32))
+        #self.toUpdate = QHBoxLayout(self.bottomWidget)
+        #self.toUpdate.setContentsMargins(0, 0, 0, 0)
+        #self.updateButton = QPushButton(u"Update", self.bottomWidget)
+        #self.toUpdate.addWidget(self.updateButton)
         # bouton de mise a jour
-        self.updateButton = QPushButton(u"Update", self.bottomWidget)
-        self.toUpdate.addWidget(self.updateButton)
+        self.updateButton = QPushButton(u"Update", self.gridWidget)
+        self.answergrid.addWidget(self.updateButton, 6, 0, 1, 6)
 
         ## gestion des slots et signaux
         self.updateButton.clicked.connect(self.update)
 
-    updatedSettings =QtCore.pyqtSignal()
+    updated = QtCore.pyqtSignal()
 
     def update(self):
         allGameNames = AccessSettings.getAllGameNames()
+        # modification dans le xml de parametres
+        AccessSettings.changeLangage(str(self.editlangue.text()))
+        AccessSettings.changeUser(str(self.edituser.text()))
+        for rank, GameName in enumerate(allGameNames):
+            AccessSettings.changeNbCards(str(self.nbcardsEdit[rank].text()),GameName)
+            AccessSettings.changeChrono(str(self.chronoEdit[rank].text()), GameName)
+        self.updated.emit()
         self.close()
 
 if __name__ == "__main__":
@@ -97,7 +111,7 @@ if __name__ == "__main__":
     w = QWidget()
     w.resize(497, 492)
     mf = mySettings(w)
-    mf.updatedSettings.connect(w.close)
+    mf.updated.connect(w.close)
     w.show()
     a.exec_()
     a.lastWindowClosed.connect(a.quit)
